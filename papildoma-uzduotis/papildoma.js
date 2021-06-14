d7c8c0dd7d3d26cf1d5fa04654c62d985fb9d41d
Justas1988 <b.justas@outlook.com>
2021-06-14 19:54:10 +0300

    kvadratai

---
+++ papildoma-uzduotis/papildoma.js
@@ -0,0 +1,32 @@
+"use strict"
+
+document.getElementById("calc").addEventListener("click", function() {
+
+let zingsniai = 0;
+let kiekis = parseInt(document.getElementById("squares").value);
+console.log("Įvesta trikampių: " + kiekis);
+
+for (let i=0; i<kiekis; i++) {
+    
+    let divElementas = document.createElement("div"); // div sukurtas, bet neatvaizduojamas
+    divElementas.classList.add("div-dydis");
+    divElementas.innerText = i + 1;                  //pElementas
+
+    if( (i+1) % 2 == 0 ) {
+        divElementas.classList.add("zalias");
+    }
+
+    document.querySelector(".container").append(divElementas);
+
+    console.log(i);
+    zingsniai++;
+
+    
+    
+}
+console.log("Zingsniai: " + zingsniai);
+} )
+
+
+
+ 
